f3b7e86d8175181834b2fa384361a9efdd5209b3
ckreutz@fdm.uni-freiburg.de <ckreutz@fdm.uni-freiburg.de>
2016-11-18 11:03:00 +0100

    Improved colors and markers

--- psPlotLHS.m
+++ psPlotLHS.m
@@ -8,6 +8,12 @@
 %   fnlabel     used for legend (and colors)
 %               fieldname of ar{} or ar{}.PerformanceStudy
 %               string or cell of strings 
+%               Default: {'name','intervention'}
+% 
+%               If more than 7  settings are plotted, the 1st fnlabel
+%               determines the marker and the 2nd fnlabel determines th
+%               color
+% 
 % Examples:
 % ars = psPerformStudies(studies,'first');
 % psPlotLHS([ars{:}],100)
@@ -31,9 +37,9 @@ if ischar(fnlabel)
 end
 
 
-cols = colormap('lines');
-
 cstr = cell(size(ars));
+labF1 = cell(size(ars));
+labF2 = cell(size(ars));
 for i=1:length(ars)
     cstr{i} = '';
     for f=1:length(fnlabel)
@@ -49,6 +55,11 @@ for i=1:length(ars)
             tmp = [fnlabel{f},'=',num2str(tmp)];
         end
         cstr{i} = [cstr{i},' ',tmp];
+        if f==1
+            labF1{i} = tmp;
+        elseif f==2
+            labF2{i} = tmp;            
+        end
     end
     cstr{i} = strrep(cstr{i},'_','\_');
     
@@ -58,6 +69,50 @@ end
 uni = unique(cstr);
 nfitmax = max(nfit);
 
+%% colors and markers:
+markers = {'.','o','x','v','^','<','>','d','s','+'};
+icol = NaN(size(uni));  % [1 x length(uni)], indicates which color is used
+imarker = NaN(size(uni));  % [1 x length(uni)], indicates which marker is used
+
+
+if length(uni)<=7
+    cols = colormap('lines');
+    % color determined by level of uni
+    for i=1:length(uni)
+        icol(strmatch(uni{i},cstr,'exact')) = i;
+    end
+    % only a single marker
+    markers = '.-';
+    imarker(:) = 1;
+elseif length(fnlabel)>=2
+    unif1 = unique(labF1);
+    unif2 = unique(labF2);
+    
+    if length(unif2)<=7
+        cols = colormap('lines');
+    else
+        cols = colormap('hsv');
+        cols = cols(round(interp1([1,length(unif2)],[1,size(cols,1)], 1:length(unif2))),:);
+    end
+    % color determined by level of labF2
+    for i=1:length(labF2)
+        icol(i) = strmatch(labF2{i},unif2,'exact');
+    end
+    % marker determined by level of labF1
+    for i=1:length(labF1)
+        imarker(i) = strmatch(labF1{i},unif1,'exact');
+    end
+else
+    cols = colormap('hsv');
+    cols = cols(round(interp1([1,length(uni)],[1,size(cols,1)], 1:length(uni))),:);
+    % each setting has different color
+    icol = 1:length(uni);
+    % only first marker used
+    imarker(:)=1;
+end
+
+
+%% do plotting
 close all
 hold on
 for i=1:length(uni)
@@ -70,7 +125,7 @@ for i=1:length(uni)
     for j=1:length(ind)
         chi2s{ind(j)} = sort(ars{ind(j)}.chi2s) - chi2min;
 %         chi2s{ind(j)}(chi2s{ind(j)}>ymax) = NaN;
-        tmp = plot(linspace(1,nfitmax,length(chi2s{ind(j)})), chi2s{ind(j)},'.-','Color',cols(i,:));
+        tmp = plot(linspace(1,nfitmax,length(chi2s{ind(j)})), chi2s{ind(j)},'-','Color',cols(icol(ind(j)),:),'Marker', markers{imarker(ind(j))});
         h(i) = tmp(1);
     end        
 end
